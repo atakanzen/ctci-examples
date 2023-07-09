0caab58d2cc9104e54390c97728f72f1a7c1ca11
Atakan Zengin <atakanzzengin@gmail.com>
2023-07-09 12:11:31 +0200

    Improve HasAllUniqueChars function with variable names and types for storage

--- arrays-and-strings/examples.go
+++ arrays-and-strings/examples.go
@@ -1,5 +1,7 @@
 package arraysandstrings
 
+// --------------------------------------------------------------------------------------------------------------
+
 // 1.1: Implement an algorithm to determine if a string has all unique characters, what if you cannot use additional data structures?
 
 // We can use a hashmap to create a frequency analysis, by iterating on each letter and see if it exists in the
@@ -7,14 +9,25 @@ package arraysandstrings
 // O(N2) since I need to check each pair one by one.
 
 func HasAllUniqueChars(input string) bool {
-	frequencyTable := make(map[rune]int, 0)
+	set := make(map[rune]bool, 0)
 
 	for _, ch := range input {
-		if _, ok := frequencyTable[ch]; ok {
+		if _, ok := set[ch]; ok {
 			return false
 		}
-		frequencyTable[ch] = 1
+		set[ch] = true
 	}
 
 	return true
 }
+
+// What I missed from the Book?
+
+/*
+	- Asking whether string is ASCII or Unicode to the interviewer
+		- This effects the storage size of the alphabet, which is 128 possible values in ASCII, and 256 in extended ASCII
+	- I couldn't think about returning directly false if the string length exceeds the possible number of unique chars
+	- Asking about whether we're allowed to modify the string to sort it and linearly checking neighboring chars
+*/
+
+// --------------------------------------------------------------------------------------------------------------
