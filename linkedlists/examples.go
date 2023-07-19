13bd553cdd3c3cbd4231881284513beb70bf5473
Atakan Zengin <atakanzzengin@gmail.com>
2023-07-19 17:01:56 +0200

    Add RemoveDups function for example 2.1 in Linked Lists

--- linkedlists/examples.go
+++ linkedlists/examples.go
@@ -1,10 +1,26 @@
 package linkedlists
 
-import "github.com/atakanzen/go-dsa/linkedlist"
+import (
+	ll "github.com/atakanzen/go-dsa/linkedlist"
+)
 
 // 2.1 Write code to remove duplicates from an unsorted linked list. Follow up, how about without an intermediary buffer
 
-func RemoveDups() {
-	dl := linkedlist.NewDoublyLinkedList(10)
-	dl.Remove(10)
+func RemoveDups(linkedList *ll.DoublyLinkedList) []interface{} {
+	frequencyMap := make(map[interface{}]int, linkedList.Length) // Additional intermediary buffer
+
+	n := linkedList.Head
+
+	for i := 0; n != nil; i++ {
+
+		if _, ok := frequencyMap[n.Value]; ok {
+			linkedList.RemoveAt(i)
+			i -= 1 // Since ith has been removed we decrement to align with new length
+		} else {
+			frequencyMap[n.Value] = 1
+		}
+		n = n.Next
+	}
+
+	return linkedList.Values()
 }
--- linkedlists/examples_test.go
+++ linkedlists/examples_test.go
@@ -1,1 +1,29 @@
 package linkedlists_test
+
+import (
+	"testing"
+
+	"github.com/atakanzen/ctci-examples/linkedlists"
+	"github.com/atakanzen/go-dsa/linkedlist"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRemoveDups(t *testing.T) {
+	testCases := []struct {
+		desc  string
+		input *linkedlist.DoublyLinkedList
+		want  []interface{}
+	}{
+		{
+			desc:  "should remove 2 duplicates",
+			input: linkedlist.NewDoublyLinkedList(15, 25, 23, 11, 25, 23),
+			want:  []interface{}{15, 25, 23, 11},
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			actual := linkedlists.RemoveDups(tC.input)
+			assert.Equal(t, tC.want, actual)
+		})
+	}
+}
